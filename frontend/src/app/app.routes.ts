e02e70b392108597a9f0a6cf4da6334b40d51fc1
LAPTOP-9LEO7DAD\zxmacoi <zxmac.dev@gmail.com>
2026-01-05 09:37:21 +0800

    Initial commit (frontend)

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
+
+export const routes: Routes = [
+  { path: '', component: Dashboard, title: 'Dashboard Page' },
+];
